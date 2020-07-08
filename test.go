367888c79b8e307205f03e893add9834519a9c78
RuchiBhardwaj <ruchi.bhardwaj@nineleaps.com>
2020-07-08 13:33:48 +0530

    creating UrlShortner

---
+++ test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	TINY_URL = 1
+	IS_GD    = 2
+)
+
+type UrlShortener struct {
+	ShortUrl    string
+	OriginalUrl string
+}
+
+func getResponseData(urlOrig string) string {
+	response, err := http.Get(urlOrig)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	return string(contents)
+}
+
+func tinyUrlShortener(urlOrig string) (string, string) {
+	escapedUrl := url.QueryEscape(urlOrig)
+	tinyUrl := fmt.Sprintf("http://tinyurl.com/api-create.php?url=%s", escapedUrl)
+	return getResponseData(tinyUrl), urlOrig
+}
+
+func isGdShortener(urlOrig string) (string, string) {
+	escapedUrl := url.QueryEscape(urlOrig)
+	isGdUrl := fmt.Sprintf("http://is.gd/create.php?url=%s&format=simple", escapedUrl)
+	return getResponseData(isGdUrl), urlOrig
+}
+
+func (u *UrlShortener) short(urlOrig string, shortener int) *UrlShortener {
+	switch shortener {
+	case TINY_URL:
+		shortUrl, originalUrl := tinyUrlShortener(urlOrig)
+		u.ShortUrl = shortUrl
+		u.OriginalUrl = originalUrl
+		return u
+	case IS_GD:
+		shortUrl, originalUrl := isGdShortener(urlOrig)
+		u.ShortUrl = shortUrl
+		u.OriginalUrl = originalUrl
+		return u
+	}
+	return u
+}
+
+func main() {
+	urlOrig := UrlShortener{}
+	urlOrig.short("http://example.com/this-is-a-very-long-url-bla-bla-bla", TINY_URL)
+	fmt.Println(urlOrig.ShortUrl)
+	fmt.Println(urlOrig.OriginalUrl)
+}
